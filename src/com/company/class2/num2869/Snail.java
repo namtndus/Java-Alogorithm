911fdcbfd7fb125460ef9ec38c189816d6c4e4a2
suyeon <rnrmf0721@gmail.com>
2022-02-20 22:46:56 +0900

    num2869 success

--- src/com/company/class2/num2869/Snail.java
+++ src/com/company/class2/num2869/Snail.java
@@ -1,4 +1,20 @@
 package com.company.class2.num2869;
 
+import java.util.Scanner;
+
 public class Snail {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int speed = sc.nextInt();
+        int down = sc.nextInt();
+        int height = sc.nextInt();
+
+        int day = (height - down) / (speed -down);
+
+        //나머지가 있을 경우 (잔여 블럭이 있을 경우)
+        if((height - down) % (speed - down) != 0){
+            day++;
+        }
+        System.out.println(day);
+    }
 }
