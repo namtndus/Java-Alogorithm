0b40f362fc0d0805477e12e51c10d1f8c50c3a43
suyeon <rnrmf0721@gmail.com>
2022-02-20 21:08:46 +0900

    num2292 success

---
+++ src/com/company/class2/num2292/Bee.java
@@ -0,0 +1,25 @@
+package com.company.class2.num2292;
+
+import java.util.Scanner;
+
+public class Bee {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int count = 1;
+        int range = 2;
+
+        if(num == 1){
+            System.out.println(1);
+        }else{
+            while(range <= num){
+                range = range + (6 * count);
+                count++;
+            }
+            System.out.println(count);
+        }
+
+    }
+}
