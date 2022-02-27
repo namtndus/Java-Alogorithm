7b27ac728fa6f1e97aca1f47d49ee5d2a40faa81
suyeon <rnrmf0721@gmail.com>
2022-02-28 01:38:44 +0900

    num2609 succes

---
+++ src/com/company/class2/num2609/MaxMin.java
@@ -0,0 +1,29 @@
+package com.company.class2.num2609;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MaxMin {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int d = gcd(a, b);
+
+        System.out.println(d);
+        System.out.println(a * b /d);
+    }
+
+    private static int gcd(int a, int b) {
+        if(b == 0){
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
