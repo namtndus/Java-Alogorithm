729a5d99aa70715ae3db9ab5940728b79cf3eba9
suyeon <rnrmf0721@gmail.com>
2022-03-03 16:40:00 +0900

    num11866 scuccess

--- src/com/company/class2/num11866/JosephusProblem.java
+++ src/com/company/class2/num11866/JosephusProblem.java
@@ -1,15 +1,34 @@
 package com.company.class2.num11866;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class JosephusProblem {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         int size = Integer.parseInt(st.nextToken());
         int num  = Integer.parseInt(st.nextToken());
 
+        int index = 0;
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= size; i++){
+            list.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('<');
+        for(int i = 0; i<size-1; i++){
+            index = (index +(num-1)) % list.size();
+            sb.append(list.remove(index)).append(", ");
+        }
+
+        sb.append(list.remove(0)).append('>');
+        System.out.println(sb);
+        br.close();
     }
 }
