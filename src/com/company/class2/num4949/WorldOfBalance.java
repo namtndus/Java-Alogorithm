66b6f9b30272d933fbb41014b04f91345a6d77f1
suyeon <rnrmf0721@gmail.com>
2022-03-10 11:21:38 +0900

    num4949 success

--- src/com/company/class2/num4949/WorldOfBalance.java
+++ src/com/company/class2/num4949/WorldOfBalance.java
@@ -1,21 +1,50 @@
 package com.company.class2.num4949;
 
 import java.io.*;
+import java.util.Stack;
 
 public class WorldOfBalance {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        String word = br.readLine();
+        String s;
 
-        while(word == "."){
-            find(word);
-            word = br.readLine();
+        while(true){
+            s = br.readLine();
+            if(s.equals(".")){
+                break;
+            }
+            sb.append(solve(s)).append("\n");
         }
+        System.out.println(sb);
     }
 
-    private static void find(String word) {
+    private static String solve(String s) {
 
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '['){
+                stack.push(c);
+            }else if(c == ')'){
+                if(stack.empty() || stack.peek() !='('){
+                    return "no";
+                }else{
+                    stack.pop();
+                }
+            }else if(c == ']'){
+                if(stack.empty() || stack.peek() != '['){
+                    return "no";
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+        if(stack.empty()){
+            return "yes";
+        }else{
+            return "no";
+        }
     }
 }
