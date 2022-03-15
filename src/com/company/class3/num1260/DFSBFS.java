c1c46d3b51f3218046f673552fef7e810c4476de
suyeon <rnrmf0721@gmail.com>
2022-03-15 14:29:10 +0900

    num1260 success

---
+++ src/com/company/class3/num1260/DFSBFS.java
@@ -0,0 +1,72 @@
+package com.company.class3.num1260;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class DFSBFS {
+
+    static int[][] check;
+    static boolean[] checked;
+    static int n;
+    static int m;
+    static int start;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(st.nextToken());
+
+        check = new int[1001][1001];
+        checked = new boolean[1001];
+
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            check[x][y] = check[y][x] = 1;
+        }
+
+        dfs(start);
+
+        checked = new boolean[1001];
+        System.out.println();
+
+        bfs();
+
+    }
+
+    public static void dfs(int i) {
+        checked[i] = true;
+        System.out.print(i+" ");
+        for(int j = 1; j <= n; j++){
+            if(check[i][j] == 1 && checked[j] ==false){
+                dfs(j);
+            }
+        }
+    }
+
+    private static void bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        checked[start] = true;
+        System.out.print(start + " ");
+
+        while(!queue.isEmpty()){
+            int temp = queue.poll();
+            for(int j = 1; j <= n; j++){
+                if(check[temp][j] == 1 && checked[j] == false){
+                    queue.offer(j);
+                    checked[j] = true;
+                    System.out.print(j + " ");
+                }
+            }
+        }
+    }
+}
