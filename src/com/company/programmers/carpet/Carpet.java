beab6dc31c121aac3111b09005e4bf7b11f2a579
suyeon <rnrmf0721@gmail.com>
2022-03-20 23:57:44 +0900

    carpet

---
+++ src/com/company/programmers/carpet/Carpet.java
@@ -0,0 +1,23 @@
+package com.company.programmers.carpet;
+
+public class Carpet {
+    static int brown = 10;
+    static int yellow = 2;
+
+    public static void main(String[] args) {
+        int[] answer = new int[2];
+        int area = brown + yellow;
+        for(int i = 1; i<= area; i++){
+            int row = i;
+            int col = area / row;
+            if(row > col)
+                continue;
+            if((row - 2) * (col - 2) == yellow){
+                answer[0] = col;
+                answer[1] = row;
+                return answer;
+            }
+            return answer;
+        }
+    }
+}
---
+++ src/com/company/programmers/dontGotheDistancePlayer/DontGotheDistancePlayer.java
@@ -0,0 +1,24 @@
+package com.company.programmers.dontGotheDistancePlayer;
+
+public class DontGotheDistancePlayer {
+    static String[] participant = {"mislav", "stanko", "mislav", "ana"};
+    static String[] completion = {"stanko", "ana", "mislav"};
+
+    public static void main(String[] args) {
+        boolean[] isChecked = new boolean[participant.length];
+        for(int i = 0; i<participant.length; i++){
+            for(int j = 0; j< completion.length; j++ ){
+                if(participant[i].equals(completion[j])){
+                    isChecked[i] = true;
+                    break;
+                }
+            }
+        }
+
+        for(int i = 0; i<isChecked.length; i++){
+            if(!isChecked[i]){
+                System.out.println(participant[i]);
+            }
+        }
+    }
+}
