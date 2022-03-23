eca8e5ab1635faabf1e2d6b73853b0a492a4c72a
suyeon <rnrmf0721@gmail.com>
2022-03-23 10:38:51 +0900

    핸드폰 번호 가리르기 프로그래머스 문제 성공

---
+++ src/com/company/programmers/blindPhoneNumber/BlindPhoneNumber.java
@@ -0,0 +1,19 @@
+package com.company.programmers.blindPhoneNumber;
+
+public class BlindPhoneNumber {
+    static String phone_number = "01033334444";
+
+    public static void main(String[] args) {
+        String answer = "";
+
+        for(int i = 0; i<phone_number.length(); i++){
+            if(i>=0 && i<phone_number.length()-4){
+                answer+="*";
+            }else{
+                answer+=phone_number.substring(i, i+1);
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
