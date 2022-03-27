a4ca3e6fc61f070eba14b5c1c7374d353006e3ea
suyeon <rnrmf0721@gmail.com>
2022-03-27 11:37:04 +0900

    자연수 뒤집어 배열로 만들기

--- src/com/company/programmers/level1/reverseNum/ReverseNum.java
+++ src/com/company/programmers/level1/reverseNum/ReverseNum.java
@@ -1,2 +1,29 @@
-package com.company.programmers.level1.reverseNum;public class ReverseNum {
+package com.company.programmers.level1.reverseNum;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ReverseNum {
+
+    public int[] solution(long n){
+
+        String[] str = String.valueOf(n).split("");
+
+        int[] answer = new int[str.length];
+        for(int i = 0; i<answer.length; i++){
+            answer[answer.length-i-1] = Integer.parseInt(str[i]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ReverseNum numw = new ReverseNum();
+        long num = 12345;
+        int[] quiz = numw.solution(num);
+
+        for(int i = 0; i < quiz.length; i++){
+            System.out.println(quiz[i]);
+        }
+    }
 }
