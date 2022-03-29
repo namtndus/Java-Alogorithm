ed3c43bd70ceeb91cbbb2aeb4a8d334774a5ae3f
suyeon <rnrmf0721@gmail.com>
2022-03-29 14:07:07 +0900

    프로그래머스 시저함수 성공

--- src/com/company/programmers/level1/divisorSum/DivisorSum.java
+++ src/com/company/programmers/level1/divisorSum/DivisorSum.java
@@ -1,2 +1,23 @@
-package com.company.programmers.level1.divisorSum;public class DivisorSum {
+package com.company.programmers.level1.divisorSum;
+
+public class DivisorSum {
+
+    public int solution(int n){
+        int answer = 0;
+        for(int i = 1; i <= Math.sqrt(n); i++ ){
+            int divisor = n/i;
+            if(n%i == 0 && n/i == i){
+                answer+= i;
+            }else if(n%i == 0){
+                answer+= i+divisor;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        DivisorSum sum = new DivisorSum();
+        System.out.println(sum.solution(12));
+    }
+
 }
