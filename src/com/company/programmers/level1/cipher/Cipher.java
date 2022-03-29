42137610aebb35ab9eb620dfb0ca70165e627def
suyeon <rnrmf0721@gmail.com>
2022-03-29 14:04:44 +0900

    프로그래머스 약수 더하기 성공

---
+++ src/com/company/programmers/level1/cipher/Cipher.java
@@ -0,0 +1,33 @@
+package com.company.programmers.level1.cipher;
+
+public class Cipher {
+    public String solution(String s, int n){
+        String answer = "";
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch ==' '){
+                answer += ch;
+                continue;
+            }
+            if(ch >='a' && ch<='z'){
+                if(ch+n > 'z'){
+                    answer += (char) (ch-26+n);
+                }else{
+                    answer += (char)(ch+n);
+                }
+            }else if(ch>='A' && ch<='Z'){
+                if(ch+n > 'Z'){
+                    answer += (char)(ch-26+n);
+                }else{
+                    answer += (char)(ch+n);
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Cipher cipher = new Cipher();
+        System.out.println(cipher.solution("AB",1));
+    }
+}
---
+++ src/com/company/programmers/level1/divisorSum/DivisorSum.java
@@ -0,0 +1,2 @@
+package com.company.programmers.level1.divisorSum;public class DivisorSum {
+}
