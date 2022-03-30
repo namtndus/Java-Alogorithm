dd9de2641a38f5cd5fdac6fcc98e733cf6be5dca
suyeon <rnrmf0721@gmail.com>
2022-03-30 11:21:38 +0900

    프로그래머스 소수 찾기 성공

--- src/com/company/programmers/level1/findprimenumber/FindPrimeNumber.java
+++ src/com/company/programmers/level1/findprimenumber/FindPrimeNumber.java
@@ -1,2 +1,32 @@
-package com.company.programmers.level1.findprimenumber;public class FindPrimeNumber {
+package com.company.programmers.level1.findprimenumber;
+
+import java.util.Arrays;
+
+public class FindPrimeNumber {
+    public int solution(int n){
+        boolean[] arr = new boolean[n+1];
+        Arrays.fill(arr,true);
+        arr[0] = false;
+        arr[1] = false;
+        int answer = 0;
+
+        for(int i = 2; i<arr.length; i++){
+            for(int j = i; j<arr.length; j+=i){
+                if(j != i){
+                    arr[j] = false;
+                }
+            }
+        }
+        for(int i = 2; i<arr.length; i++){
+            if(arr[i] == true){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FindPrimeNumber primeNumber = new FindPrimeNumber();
+        System.out.println(primeNumber.solution(10));
+    }
 }
