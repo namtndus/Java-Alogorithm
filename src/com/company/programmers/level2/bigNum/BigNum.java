0e0128bb1b9c3f0c05d19558f5822b3d3c6804b6
suyeon <rnrmf0721@gmail.com>
2022-04-01 11:59:05 +0900

    프로그래머스 가장 큰 수 실패 다른 사람이 쓴 코드임

--- src/com/company/programmers/level2/bigNum/BigNum.java
+++ src/com/company/programmers/level2/bigNum/BigNum.java
@@ -1,29 +1,37 @@
 package com.company.programmers.level2.bigNum;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class BigNum {
 
-    static int[] numbers = {6,10,2};
+    public String solution(int[] numbers){
+        String answer = "";
+        String[] strNumber = new String[numbers.length];
+
+        for(int i = 0; i< strNumber.length; i++){
+            strNumber[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(strNumber, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return (s2+s1).compareTo(s1+s2);  // 내림차순 정렬
+            }
+        });
+
+        for(String tmp :strNumber){
+            answer += tmp;
+        }
+        if(answer.startsWith("0")) answer = "0";
+
+        return answer;
+    }
 
     public static void main(String[] args) {
-
-        String[] stringNumbers = new String[numbers.length];
-
-        for(int i = 0; i< numbers.length; i++){
-            stringNumbers[i] = Integer.toString(numbers[i]);
-        }
-
-        Arrays.sort(stringNumbers, (o1, o2) -> (o2+o1).compareTo(o1+o2));
-
-        if(stringNumbers[0].equals("0")){
-            System.out.println(0);
-            return ;
-        }
-
-
-        System.out.println(String.join("",stringNumbers));
-
+        BigNum bigNum = new BigNum();
+        int[] arg = {6,10,2};
+        System.out.println(bigNum.solution(arg));
     }
 
 }
