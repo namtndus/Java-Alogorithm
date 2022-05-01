01bc2fa320e4092f4e11eba8b5f46c15121e245f
suyeon <rnrmf0721@gmail.com>
2022-05-01 23:17:16 +0900

    프로그래머스 조이스틱 수정

--- src/com/company/programmers/level2/joystick/Joystick.java
+++ src/com/company/programmers/level2/joystick/Joystick.java
@@ -2,21 +2,28 @@ package com.company.programmers.level2.joystick;
 
 public class Joystick {
 
+    static int count = 0;
+
     public int solution(String name){
-        int count = 0;
+
+        boolean[] isChecked = new boolean[name.length()];
+        int[] x = new int[]{1,-1};
         char answer = 'A';
         char[] str = name.toCharArray();
-        for(char i : str){
-            int num = i - answer;
-            if(num >= 13){
-                num = Math.abs(num-26);
-            }
-            count += num;
-        }
+
+        changeChar(x,str,answer,isChecked,0);
 
         count += name.length()-1;
 
         return count;
+    }
+
+    private void changeChar(int[] x, char[] str, char answer,boolean[] isChecked, int order) {
+        int num = str[order] - answer;
+        if(num >= 13){
+            num = Math.abs(num-26);
+        }
+        
     }
 
     public static void main(String[] args) {
