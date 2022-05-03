ff814e19aace77437edab08df1cdc778fed6a6ec
suyeon <rnrmf0721@gmail.com>
2022-05-04 00:31:22 +0900

    프로그래머스 전화번호 목록 해결중

--- src/com/company/programmers/level2/phoneNumberList/PhoneNumberList.java
+++ src/com/company/programmers/level2/phoneNumberList/PhoneNumberList.java
@@ -1,17 +1,16 @@
 package com.company.programmers.level2.phoneNumberList;
 
+import java.util.Arrays;
+
 public class PhoneNumberList {
 
     public boolean solution(String[] phone_book){
         boolean answer = true;
+        Arrays.sort(phone_book);
 
-        for(int i = 0; i< phone_book.length; i++){
-            for(int j = i+1; j< phone_book.length; j++){
-                if(phone_book[i].length() < phone_book[j].length()){
-                    if(phone_book[i].equals(phone_book[j].substring(0, phone_book[i].length()))){
-                        return false;
-                    }
-                }
+        for(int i = 0; i < phone_book.length-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
             }
         }
 
