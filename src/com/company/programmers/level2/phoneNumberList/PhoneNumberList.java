1e3e248870bd37cfd63eedcc8560b123aff65462
suyeon <rnrmf0721@gmail.com>
2022-05-04 22:40:43 +0900

    프로그래머스 전화번호 목록 해쉬중

--- src/com/company/programmers/level2/phoneNumberList/PhoneNumberList.java
+++ src/com/company/programmers/level2/phoneNumberList/PhoneNumberList.java
@@ -1,20 +1,27 @@
 package com.company.programmers.level2.phoneNumberList;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PhoneNumberList {
 
     public boolean solution(String[] phone_book){
-        boolean answer = true;
-        Arrays.sort(phone_book);
 
-        for(int i = 0; i < phone_book.length-1; i++){
-            if(phone_book[i+1].startsWith(phone_book[i])){
-                return false;
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < phone_book.length; i++){
+            map.put(phone_book[i],i);
+        }
+
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = 0; j < phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0,j))){
+                    return false;
+                }
             }
         }
 
-        return answer;
+        return true;
     }
 
     public static void main(String[] args) {
