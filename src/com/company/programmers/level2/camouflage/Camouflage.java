678a69911a6f36199bef503f8ee42d3b69c366f1
suyeon <rnrmf0721@gmail.com>
2022-05-06 16:43:40 +0900

    프로그래머스 해시 위장 성공

--- src/com/company/programmers/level2/camouflage/Camouflage.java
+++ src/com/company/programmers/level2/camouflage/Camouflage.java
@@ -2,11 +2,12 @@ package com.company.programmers.level2.camouflage;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class Camouflage {
     public int solution(String[][] clothes){
         HashMap<String, ArrayList<String>> clothesList = new HashMap<>();
-        int answer = 0;
+        int answer = 1;
 
         for(int i = 0; i< clothes.length; i++){
             if(!clothesList.containsKey(clothes[i][1]))
@@ -14,11 +15,13 @@ public class Camouflage {
             clothesList.get(clothes[i][1]).add(clothes[i][0]);
         }
 
+        Iterator<String> keys = clothesList.keySet().iterator();
+        while(keys.hasNext()){
+            answer *= (clothesList.get(keys.next()).size() +1);
+        }
 
 
-        System.out.println(clothesList.toString());
-
-        return answer;
+        return answer - 1;
     }
 
     public static void main(String[] args) {
@@ -26,6 +29,6 @@ public class Camouflage {
         String[][] clothes2 = new String[][]{{"crowmask", "face"},{"bluesunglasses", "face"},{"smoky_makeup","face"}};
         //System.out.println(Arrays.deepToString(clothes2));
         Camouflage camouflage = new Camouflage();
-        camouflage.solution(clothes1);
+        System.out.println(camouflage.solution(clothes2));
     }
 }
