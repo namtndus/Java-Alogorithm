eaee2e3d8bf089a9f2b8db064ee03a36eb2efd55
suyeon <rnrmf0721@gmail.com>
2022-05-14 23:55:35 +0900

    프로그래머스 주식가격 큐 생성

--- src/com/company/programmers/level2/stockPrices/StockPrices.java
+++ src/com/company/programmers/level2/stockPrices/StockPrices.java
@@ -1,43 +1,17 @@
 package com.company.programmers.level2.stockPrices;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class StockPrices {
 
     public int[] solution(int[] prices){
-        int[] answer = new int[prices.length];
-        ArrayList<Integer> queue = new ArrayList<>();
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
-        for(int i : prices){
-            queue.add(i);
+        int[] answer = {};
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < prices.length; i++){
+            queue.add(prices[i]);
         }
-
-        while(!queue.isEmpty()){
-            int i = 0;
-            int output = queue.remove(0);
-
-            if(queue.isEmpty()){
-                arrayList.add(0);
-                break;
-            }
-            while(i<queue.size()){
-                if(output > queue.get(i)){
-                    break;
-                }
-                i++;
-            }
-            if(i>=queue.size()){
-                arrayList.add(i);
-            }else{
-                arrayList.add(i+1);
-            }
-        }
-
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = arrayList.get(i);
-        }
-
         return answer;
     }
 
